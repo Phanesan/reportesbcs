c711a60eee69ee72ae5871bc1885c4bac2088faf
Aleksey Unzon <rubenalekseyunzon@gmail.com>
2023-12-02 02:53:09 -0700

    Cree la pagina principal

--- Front-End/reportesbcs/reportesbcs/views.py
+++ Front-End/reportesbcs/reportesbcs/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse 
 
-# Request 
-# HttpResponse
+# Create your views here.
 
-def bienvenida(request)
-    return HttpResponse("hola")
+def bienvenida(request):
+    return render(request, 'index.html')
