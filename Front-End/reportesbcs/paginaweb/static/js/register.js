3050abb9013c082ff7e11c425e08590805d1ae89
Aleksey Unzon <rubenalekseyunzon@gmail.com>
2023-12-04 12:17:23 -0700

    Añadi la funcion register

--- Front-End/reportesbcs/paginaweb/static/js/register.js
+++ Front-End/reportesbcs/paginaweb/static/js/register.js
@@ -29,3 +29,22 @@ document.querySelector("#activarAgreagarImagen").addEventListener("click", () =>
       )
     }
   })
+
+  document.getElementById("RegistrarUsuario").addEventListener("submit",async(e)=>{
+    e.preventDefault()
+    console.log(e)
+    const res = await fetch("http://127.0.0.1:8000/api/register", {
+      method: "POST",
+      headers:{
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify({
+        nombre: e.target.exampleFormNameInput1.value,
+        apellido: e.target.exampleFormLastNameInput1.value,
+        curp: e.target.exampleFormCurpInput1.value,
+        claveElector:e.target.exampleFormClaveElectorInput1.value,
+        correo: e.target.exampleFormControlInput1.value,
+        contraseña: e.target.inputPassword5.value
+      })
+    })
+  })
